f980b5e1a271eadf29d19ae61da4e47fc1ab0525
ansaris <salman.ansari.usa@gmail.com>
2016-05-24 09:18:27 -0500

    Add Hangman, Outcome, and Player class. Add properties for Hangman, WordList, Word, Outcome, and Guess.

---
+++ IntermediateCS_Video_11/Player.cs
@@ -0,0 +1,12 @@
+﻿class Player
+{
+    // This is a read-write property of type string
+    // called Name.
+
+    // We'll look at properties in more detail in a 
+    // later video.
+
+    // Why public? We'll look at that later as well.
+
+    public string Name { get; set; }
+}
